ced2d40584c221269a71ec3ace4f79bb9b39bd13
Hasinur <hasinur.rahman1332@gmail.com>
2024-12-22 08:56:28 +0600

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+import daisyui from "daisyui";
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: "#4A90E2 ",
+        primaryAccent: "#1d62b4",
+        backGround: "#F5F5F5",
+        darkBg: "#1E1E2C",
+        darkText: "#EDEDED",
+      },
+      fontFamily: {
+        lato: ["Lato", "serif"],
+      },
+    },
+  },
+  plugins: [daisyui],
+};
